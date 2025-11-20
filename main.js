0577a1041f0366ca809a4a6055ce24104e325dfa
Tomasz Turek <turek9933@gmail.com>
2025-11-20 11:36:36 +0100

    Dodanie obsługi nagrywania oraz bazy danych IndexedDB, przechowującej notatki oraz opcjonalnie audio. Wystylizowanie wyglądu, dodanie fontu.

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 const recordBtn = document.getElementById('recordBtn');
+const saveBtn = document.getElementById('saveBtn');
 const noteText = document.getElementById('noteText');
-const logDiv = document.getElementById('test');
+const audioDiv = document.getElementById('audio-wrapper');
 
 let mediaRecorder;
-let recognition;
+let recognition = new SpeechRecognition();
 let audioChunk = [];
-
-function log(message) {
-    const logEntry = document.createElement('p');
-    logEntry.textContent = message;
-    logDiv.appendChild(logEntry);
-}
+let lastRecordedBlob = null;
 
 
 if ('webkitSpeechRecognition' in window) {
@@ -18,25 +14,24 @@ if ('webkitSpeechRecognition' in window) {
     recognition.lang = 'pl-PL';
     recognition.continuous = true;
     recognition.interimResults = true;
-    recognition.onstart = () => console.log('[webkitSpeechRecognition] Rozpoczynam nagrywanie.');
-    recognition.onend = () => console.log('[webkitSpeechRecognition] Koniec nagrywania.');
 
     recognition.onresult = (e) => {
-        alert(e.results[0][0].transcript);
-        const transcript = e.results[0][0].transcript;
-        noteText.value = transcript;
-        console.log(`Transkrypcja: ${transcript}`);
-        log(`Transkrypcja: ${transcript}`);
+        let finalTranscript = '';
+        for (let i = 0; i < e.results.length; i++) {
+            console.log(e.results[i][0].transcript);
+            finalTranscript = finalTranscript + e.results[i][0].transcript.trim() + '\n';
+        };
+        noteText.value = finalTranscript;
     };
 }
 
 // Nagrywanie audio
 recordBtn.addEventListener('click', async () => {
-    console.log(mediaRecorder);
+    if(mediaRecorder) console.log(mediaRecorder.state);
     if (!mediaRecorder || mediaRecorder.state === 'inactive') {
         try {
             //TODO Dodać weryfikację połączenia oraz inicjalizację rozpoznawania mowy
-            // if (recognition) recognition.start();
+            if (recognition) recognition.start();
             
             const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
             mediaRecorder = new MediaRecorder(stream);
@@ -50,12 +45,18 @@ recordBtn.addEventListener('click', async () => {
 
             mediaRecorder.onstop = async () => {
                 const blob = new Blob(audioChunk, { type: 'audio/wav' });
-                
+                lastRecordedBlob = blob;
+
                 const audioUrl = URL.createObjectURL(blob);
                 const audio = new Audio(audioUrl);
 
                 audio.controls = true;
-                logDiv.appendChild(audio);
+                if (audioDiv.firstChild) {
+                    audioDiv.removeChild(audioDiv.firstChild);
+                    audioDiv.style.display = "none";
+                }
+                audioDiv.appendChild(audio);
+                audioDiv.style.display = "block";
                 audio.play();
 
                 audioChunk = [];
@@ -69,8 +70,162 @@ recordBtn.addEventListener('click', async () => {
             return;
         }        
     } else {
-        // if (recognition) recognition.stop();
+        if (recognition) recognition.stop();
         mediaRecorder.stop();
         recordBtn.textContent = 'Rozpocznij nagrywanie';
+    }
+});
+
+
+const openRequest = indexedDB.open('test', 5);
+let db;
+
+const testData = [
+    { name: 'ford', vin: 1234567, year: 1999, color: 'red' },
+    { name: 'fiat', vin: 4567890, year: 1998, color: 'purple' },
+    { name: 'opel', vin: 2345678, year: 2002, color: 'blue' },
+    { name: 'fiat', vin: 3456789, year: 2004, color: 'green' },
+];
+
+
+openRequest.onerror = function() {
+    console.error("DB Error", openRequest.error);
+};
+openRequest.onsuccess = (e) => {
+    db = e.target.result;
+    console.log("DB opened succesfully");
+};
+
+openRequest.onupgradeneeded = (e) => {
+    db = e.target.result;
+    if (!db.objectStoreNames.contains("cars")) {
+        db.deleteObjectStore("cars");
+    
+        const objectStore = db.createObjectStore("cars", { keyPath: 'vin'});
+    
+        objectStore.createIndex("name", "name", { unique: false });
+    
+        objectStore.transaction.oncomplete = (event) => {
+            const carsObjectStore = db
+            .transaction("cars", "readwrite")
+            .objectStore("cars");
+    
+            testData.forEach((car) => {
+                carsObjectStore.add(car);
+            });
+        };
+    }
+
+
+    const objectStoreNotes = db.createObjectStore("notes", { keyPath: 'id'});
+    const objectStoreAudio = db.createObjectStore("audio", { keyPath: 'id'});
+
+    objectStoreAudio.createIndex("noteId", "noteId", { unique: true });
+
+};
+
+// testBtn.addEventListener("click", async () => {
+//     const tx = db.transaction("cars", "readonly");
+//     const store = tx.objectStore("cars");
+//     const index = store.index("name");
+//     // index.get("fiat").onsuccess = (e) => {
+//     //     console.log('index success');
+//     //     console.log(e.target.result);
+//     // }
+//     let i = 0;
+//     const findMyFiat = IDBKeyRange.only("fiat");
+//     index.openCursor(findMyFiat).onsuccess = (e) => {
+//         const cursor = e.target.result;
+//         if (cursor) {
+//             console.log('cursor success');
+//             console.log(cursor.value);
+//             i += 1;
+//             console.log(i);
+//             cursor.continue();
+//         }
+//     };
+// });
+
+// testBtn1.addEventListener("click", async () => {
+//     db
+//     .transaction("cars")
+//     .objectStore("cars")
+//     .get(1234567)
+//     .onsuccess = (e) => {
+//         console.log("request success 1");
+//         console.log(e.target.result);
+//         // console.log(e.target);
+//     }
+// });
+
+function saveNote() {
+    const checked = document.getElementById('checkbox').checked;
+    const noteId = crypto.randomUUID();
+    const audioId = crypto.randomUUID();
+
+    console.log(
+        {
+            id: noteId,
+            note: noteText.value.trim(),
+            hasAudio: !!checked,
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+    );
+
+    db
+    .transaction("notes", "readwrite")
+    .objectStore("notes")
+    .add({
+        id: noteId,
+        note: noteText.value.trim(),
+        hasAudio: !!checked,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    })
+    .onsuccess = ((e) => {
+        console.log("Note added");
+    })
+
+    if (!!checked) {
+        db
+        .transaction("audio", "readwrite")
+        .objectStore("audio")
+        .add({
+            id: audioId,
+            noteId: noteId,
+            blob: lastRecordedBlob
+        });
+        // TODO
+        idLastRecordedNote = noteId;
+    }
+}
+
+
+saveBtn.addEventListener('click', () => {
+    console.log("trying to save note");
+    saveNote();
+});
+
+let idLastRecordedNote = null;// TODO
+const testBtn = document.getElementById('testBtn');
+testBtn.addEventListener("click", async () => {
+    if (!!idLastRecordedNote) {
+        db
+        .transaction("audio", "readonly")
+        .objectStore("audio")
+        .index("noteId")
+        .get(idLastRecordedNote)
+        .onsuccess = (e) => {
+            const blob = e.target.result.blob;
+            const audioUrl = URL.createObjectURL(blob);
+            const audio = new Audio(audioUrl);
+
+            audio.controls = true;
+
+            const testAudioDiv = document.getElementById('test-audio-wrapper');
+            testAudioDiv.appendChild(audio);
+            testAudioDiv.style.display = "block";
+        }
     }
 });
